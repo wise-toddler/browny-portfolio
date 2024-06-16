843f11e2b4336946380b9cf7d4c923e0e38e9334
wise-toddler <wiseeldrich2004@gmail.com>
2024-06-16 23:57:43 +0530

    refactor: Remove unused code and update client information in Clients component

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -1,4 +1,5 @@
 import '../styles.css';
+import scaler_logo from '../assets/scaler-logo.svg';
 
 const Clients = () => (
   <section id="clients" className="clients-section">
@@ -12,26 +13,11 @@ const Clients = () => (
       <div className="row">
         <div className="col-lg-4 col-sm-6">
           <div className="client-item">
-            <img src="assests/client1-logo.jpg" className="img-fluid" alt="Acme Corporation" />
-            <h4>Acme Corporation</h4>
-            <p className="text-muted">Developed a responsive e-commerce website, improving sales by 20%.</p>
+            <img src={scaler_logo} className="img-fluid" alt="Client" style={{ height: '40px', width: '100px' }} />
+            <h4>Scaler</h4>
+            <p className="text-muted">Under Innovation Lab of Scaler school Of technology we developed a SFU server for scaler school of technology for handling the the bottle neck which was created due to mutiple devices tring to fetch same class from agora servers</p>
           </div>
         </div>
-        <div className="col-lg-4 col-sm-6">
-          <div className="client-item">
-            <img src="assests/client2-logo.jpg" className="img-fluid" alt="Globex Industries" />
-            <h4>Globex Industries</h4>
-            <p className="text-muted">Implemented a secure customer data management system, reducing data breaches to zero.</p>
-          </div>
-        </div>
-        <div className="col-lg-4 col-sm-6">
-          <div className="client-item">
-            <img src="assests/client3-logo.jpg" className="img-fluid" alt="Soylent Corp" />
-            <h4>Soylent Corp</h4>
-            <p className="text-muted">Optimized website SEO, increasing organic traffic by 30%.</p>
-          </div>
-        </div>
-        {/* Add more clients similarly */}
       </div>
     </div>
   </section>
